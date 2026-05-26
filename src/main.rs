537d11519ce6cf635ff84e9b739b71e4b2b3a6d2
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2026-05-26 22:13:09 +0530

    feat(player) :develop the moving player

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
-use bracket_lib::terminal::{BError, main_loop};
-use crate::map::prelude::*;
-use crate::map::map::Map; 
+use crate::prelude::*;
 
-mod map ; 
+mod map;
+mod player;
 
-struct State { 
-    map : Map , 
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::player::*;
 }
 
-impl State { 
-    fn new () -> Self { 
-        Self { map : Map::new() }
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 
-impl GameState for State { 
-     fn tick(&mut self , ctx : &mut BTerm ) { 
-        ctx.cls() ; 
-        self.map.render(ctx) ; 
-     }
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
 }
 
-fn main() -> BError { 
+fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-    .with_title("Dungeon Crawler")
-    .with_fps_cap(30.0)
-    .build()?;
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
 
     main_loop(context, State::new())
 }
