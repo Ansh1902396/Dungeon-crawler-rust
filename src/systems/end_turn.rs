50d6ec2a820572a51b23b0cd7837cea407dfcc99
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2026-05-29 16:45:40 +0530

    feat: victory state and amulet of yala

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -2,10 +2,14 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(Health)]
+#[read_component(Point)]
 #[read_component(Player)]
-pub fn end_turn( ecs : &SubWorld, #[resource] turn_state: &mut TurnState) {
-    let mut player_hp = <&Health>::query().filter(component::<Player>()); 
+#[read_component(AmuletOfYala)]
+pub fn end_turn( ecs : &SubWorld, # [resource] turn_state: &mut TurnState) {
+    let mut player_hp = <(&Health , &Point)>::query().filter(component::<Player>()); 
     let current_state = turn_state.clone() ; 
+    let mut amulet = <&Point>::query()
+        .filter(component::<AmuletOfYala>());
 
     let mut  new_state = match turn_state {
         TurnState::AwaitingInput => return,
@@ -14,9 +18,18 @@ pub fn end_turn( ecs : &SubWorld, #[resource] turn_state: &mut TurnState) {
         _=> current_state
     };
 
-    player_hp.iter(ecs).for_each(|hp| { 
+    let amulet_pos = amulet
+        .iter(ecs)
+        .nth(0)
+        .unwrap(); 
+
+    player_hp.iter(ecs).for_each(|(hp , pos)| { 
         if hp.current < 1 { 
             new_state = TurnState::GameOver ; 
+        }
+
+        if pos == amulet_pos { 
+            new_state = TurnState::Victory;
         }
     });
 
